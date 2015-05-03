92b83c00d2a03c1799a9715eaa0d11b985aac82c
Elijah Caine <elijahcainevm@gmail.com>
2015-05-03 11:16:48 -0700

    modified setup.py so it actually installs

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open('requirements.txt') as f:
     requirements = [l.strip() for l in f]
@@ -9,7 +9,12 @@ setup(
     packages=['wikibot'],
     author='Elijah Caine',
     author_email='elijahcainemv@gmail.com',
-    url='https://github.com/elijahcaine/wikibot',
+    url='https://github.com/elijahcaine/hamper-wikibot',
     install_requires=requirements,
-    package_data={'cah': ['requirements.txt', 'README.md', 'LICENSE']}
+    package_data={'cah': ['requirements.txt', 'README.md', 'LICENSE']},
+    entry_points = {
+        'hamperbot.plugins': [
+            'wikibot = wikibot.wiki:WikiBot'
+        ]
+    }
 )
