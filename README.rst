8d95ece131b12a64584e86ec53a9b38f43d627d5
Elijah Caine <elijahcainevm@gmail.com>
2015-05-03 17:23:21 -0700

    I may have broken thing, OR it's ready for production!

--- README.rst
+++ README.rst
@@ -1,13 +1,13 @@
-Wikibot Plugin - a [Hamper][1] plugin to easily make wikipedia searches.
+Wikibot Plugin - a `Hamper`_ plugin to easily make wikipedia searches.
 
 INSTALL
 =======
-First, get setup with an instance of [Hamper][1] either with docker, in a VM,
+First, get setup with an instance of `Hamper`_ either with docker, in a VM,
 or on your computer. then run:
 
-..code:: bash
+.. code:: bash
 
     pip install git+https://github.com/johnsdea/hamper-poll.git
 
 
-[1]: https://github.com/hamperbot/hamper
+.. _Hamper: https://github.com/hamperbot/hamper
--- wikibot/wiki.py
+++ wikibot/wiki.py
@@ -10,15 +10,30 @@ class WikiBot(ChatCommandPlugin):
         regex = 'wiki (.*)'
 
         def command(self, bot, comm, groups):
-            they_said = groups[0]
-            bot.reply(comm, (self.first_paragraph(they_said, comm)))
+            """
+            !wiki <query> -> wiki summary of <query>
+            """
+            query = groups[0]
+            bot.reply(comm, (self.summary(query, comm)))
 
-        def first_paragraph(self, query, comm):
+        def summary(self, query, comm):
+            """Returns wikipedia summary of <query>
+            Uses wikipedia api to grab the first <280 characters of <query>
+            article (or lets you know the query is ambigious) and appends a
+            clickable url at the end.
+            """
+            # Makes the api call
             r = re.get('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+query)
-            p = json.loads(r.text.encode('ascii','ignore'))
+            # loads r.text into an object
+            p = json.loads(r.text)
+            # pageid is needed to grab the info of a page, it's an api thing
             pageid = p['query']['pages'].keys()[0]
+            # extract is the top content of a given wiki page
             extract = p['query']['pages'][pageid]['extract']
+            # generates psuedo-slugified url
             url = 'https://en.wikipedia.org/wiki/'+query.replace(' ','_')
+            # if the article introduction is longer than 280 charcters it
+            # shortens it so you don't get wikibot spam
             try:
                 return (comm['user']+': '+extract[:280].replace(os.linesep,'\ ')+'[...] :: URL: '+url)
             except:
