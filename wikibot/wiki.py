b522b57b56280f47a1867541350a4335a25059fd
Elijah Caine <elijahcainevm@gmail.com>
2015-05-04 18:58:15 -0700

    added flags

--- wikibot/wiki.py
+++ wikibot/wiki.py
@@ -16,24 +16,78 @@ class WikiBot(ChatCommandPlugin):
             query = groups[0]
             bot.reply(comm, (self.summary(query, comm)))
 
-        def summary(self, query, comm):
+            # Prints help if requested
+            if 'help' in flags(query)[1]:
+                bot.reply(comm, (self.summary(query, comm)))
+
+
+        def summary(self, query, comm, bot):
             """Returns wikipedia summary of <query>
+
             Uses wikipedia api to grab the first <280 characters of <query>
             article (or lets you know the query is ambigious) and appends a
             clickable url at the end.
             """
+            # Generates list of flags and strips out said flags from input
+            query, flags = flags(query)
+
             # Makes the api call
             r = re.get('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+query)
-            # loads r.text into an object
+
+            # Loads r.text into an object
             p = json.loads(r.text)
-            # pageid is needed to grab the info of a page, it's an api thing
+
+            # Pageid is needed to grab the info of a page, it's an api thing
             pageid = p['query']['pages'].keys()[0]
             if pageid == '-1':
                 return (comm['user']+": I couldn't find an article for "+query)
-            # extract is the top content of a given wiki page
+
+            # Extract is the top content of a given wiki page
             extract = p['query']['pages'][pageid]['extract']
-            # generates psuedo-slugified url
+
+            # Generates psuedo-slugified url
             url = 'https://en.wikipedia.org/wiki/'+query.replace(' ','_')
-            # if the article introduction is longer than 280 charcters it
+
+            # If the article introduction is longer than 280 charcters it
             # shortens it so you don't get wikibot spam
-            return (comm['user']+': '+extract[:280].replace(os.linesep,'\ ')+'[...] :: URL: '+url)
+            if 'long' in flags:
+                return (comm['user']+': '+extract.replace(os.linesep,'\ ')+'[...] :: URL: '+url)
+            else:
+                return (comm['user']+': '+extract[:280].replace(os.linesep,'\ ')+'[...] :: URL: '+url)
+
+
+        def flags(query):
+            """Returns a modified query and list of flags
+
+            Takes a search query and returns a list of flags as well as the
+            query stripped of it's query text.
+            """
+            flags = []
+
+            # print helptext and return if no search query is passed
+            if query is None:
+                flags.append('help')
+                return query, flags
+
+            # add the help flag if it is requested
+            if '--help' in query :
+                flags.append('help')
+                query = query.replace('--help','').('-h','')
+                
+            # add longprint flag if requested
+            if '-long' in query or '-l' in query:
+                flags.append('long')
+                query = query.replace('--long','').replace('-l','')
+
+            return query, flags
+
+
+        def print_helptext():
+            """Reutrns Wikibot Docstring"""
+            helptext = "Wikibot Plugin :: \
+                        `--long` -> prints entire summary of a given article :: \
+                        `--help` -> prints out this help text :: \
+                        All flags can be passed in addition to a search query \
+                        Null input is interpreted as '--help'
+                        * Please be kind and do not spam the channel <3 *"
+            return helptext
